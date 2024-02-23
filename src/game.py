705fa6768a75ea3d73c7daf3f21d57c00c1e429e
KevinFlanagan <flanagank777@gmail.com>
2024-02-23 13:43:08 +0000

    Func: Added messages and display of 1st hangman stage to start function.

--- src/game.py
+++ src/game.py
@@ -1,2 +1,12 @@
+import random
+from src.ascii_art import hangman_stages
+from src.words import words
+from src.clear import clear_screen
+
 def start_game():
-        print("Welcome to Hangman!\n")
+    clear_screen()
+    print("Welcome to Hangman!")
+    print("Try to guess the word by guessing one letter at a time.")
+    print(hangman_stages[0])
+
+    
--- src/words.py
+++ src/words.py
@@ -1,5 +1,1 @@
-import random
-
-def choose_word():
-    word_list = ["apple", "banana", "orange", "grape", "pineapple"]
-    return random.choice(word_list)
+words = ["apple", "banana", "orange", "grape", "pineapple"]
