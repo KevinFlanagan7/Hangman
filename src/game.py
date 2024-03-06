f535d42aec1858dc76ee87f369512296781a843d
KevinFlanagan <flanagank777@gmail.com>
2024-03-06 13:53:08 +0000

    Func: Add docstring, comments and reduce lenght of lines in game file.

--- src/game.py
+++ src/game.py
@@ -4,30 +4,35 @@ from src.validation import validate_guess
 from src.api import get_words_from_api
 from colorama import Fore, Style
 
+
 def start_game(level):
+    """
+    Starts the hangman game with the specified level selected.
+    """
     clear_screen()
-    print(Fore.YELLOW + "Try to guess the word by guessing one letter at a time." + Style.RESET_ALL)
+    Info = "Try to guess the word by guessing one letter at a time."
+    print(Fore.YELLOW + Info + Style.RESET_ALL)
 
     secret_word = get_words_from_api(level)
-    print(secret_word)
-    
+
     # display secret word as underscores with spaces
     display_word = " ".join(["_" for char in secret_word])
-   
+
     guessed_letters = []
     incorrect_guesses = 0
-    max_attempts = len(STAGES) - 1  
+    max_attempts = len(STAGES) - 1
     print(STAGES[incorrect_guesses])
-    
+
     while True:
         print("Word to guess:", display_word)
-        print(Fore.YELLOW + "\nGuessed letters:", ", ".join(guessed_letters) + Style.RESET_ALL)
+        Guessed = ', '.join(guessed_letters)
+        print(f"{Fore.YELLOW}\nGuessed letters: {Guessed}{Style.RESET_ALL}")
 
         while True:
             guess = input("Enter a letter and press enter: ").lower()
 
             if validate_guess(guess, guessed_letters):
-                break        
+                break
 
         guessed_letters.append(guess)
 
@@ -36,14 +41,19 @@ def start_game(level):
             clear_screen()
             print(Fore.GREEN + "Correct guess!" + Style.RESET_ALL)
             print("Attempts remaining:", max_attempts - incorrect_guesses)
-            # Update the display word to reveal the guessed letters
-            display_word = " ".join([char if char in guessed_letters else "_" for char in secret_word])
+            # Update the displayed word to reveal the guessed letters
+            display_chars = (
+                    char if char in guessed_letters else "_"
+                    for char in secret_word
+                    )
+            display_word = " ".join(display_chars)
 
             print(STAGES[incorrect_guesses])
 
         # Check if the player has guessed all the letters correctly
-            if display_word.replace(" ","") == secret_word:
-                print(Fore.YELLOW + "Congratulations! You've guessed the word:", secret_word + Style.RESET_ALL)
+            if display_word.replace(" ", "") == secret_word:
+                Congrats = "Congratulations! You've guessed the word:"
+                print(Fore.YELLOW + Congrats, secret_word + Style.RESET_ALL)
                 break
 
         else:
@@ -57,8 +67,9 @@ def start_game(level):
             else:
                 print(Fore.RED + STAGES[incorrect_guesses] + Style.RESET_ALL)
 
-            # Check if the player has used all their attempts and displays secret word
+            # Checks if attempts have run out
             if incorrect_guesses == max_attempts:
-                print(Fore.RED + "Game over, you've run out of attempts!" + Style.RESET_ALL)
+                Game_Over = "Game over, you've run out of attempts!"
+                print(Fore.RED + Game_Over + Style.RESET_ALL)
                 print("The word was:", secret_word)
                 break
